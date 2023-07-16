46be0b5bb124a68fa1ab5ecde0548f25d2a33dbe
Christian Dimitri <christian.j.dimitrii@gmail.com>
2023-07-17 01:19:46 +0200

    rhino3dm grasshopper integrated to vue3 vite

--- src/main.js
+++ src/main.js
@@ -3,11 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Rhino3dmLoader } from 'three/examples/jsm/loaders/3DMLoader'
 const app = createApp(App)
-
 app.use(router)
 
 window.rhino3dm().then(async (rhino) => {
@@ -16,20 +12,8 @@ window.rhino3dm().then(async (rhino) => {
     console.log('Rhino3dm loaded')
   }
   if (window.RhinoCompute != null) {
-    app.config.globalProperties.$RhinoCompute = window.RhinoCompute
+    app.config.globalProperties.$rhinoCompute = window.RhinoCompute
     console.log('Rhino.Compute', window.RhinoCompute.version)
-  }
-  if (THREE != null) {
-    app.config.globalProperties.$THREE = THREE
-    console.log('THREE loaded')
-  }
-  if (OrbitControls != null) {
-    app.config.globalProperties.$OrbitControls = OrbitControls
-    console.log('OrbitControls loaded')
-  }
-  if (Rhino3dmLoader != null) {
-    app.config.globalProperties.$Rhino3dmLoader = Rhino3dmLoader
-    console.log('Rhino3dmLoader loaded')
   }
   app.mount('#app')
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,68 @@
 <!-- eslint-disable no-unused-vars -->
 
 <script>
-import { computed, ref } from 'vue'
+import { shallowRef } from 'vue'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Rhino3dmLoader } from 'three/examples/jsm/loaders/3DMLoader'
+
 export default {
   setup(props, context) {
     console.log('Setup')
-    const definition = null
-    let scene = null
-    let camera = null
-    let renderer = null
-    let doc = null
-    return { definition, scene, camera, renderer, doc }
+    var rhino3dm = shallowRef(null)
+    var three = shallowRef(null)
+    var orbitControls = shallowRef(null)
+    var rhino3dmLoader = shallowRef(null)
+    var definition = shallowRef(null)
+    var scene = shallowRef(null)
+    var camera = shallowRef(null)
+    var renderer = shallowRef(null)
+    var doc = shallowRef(null)
+    return {
+      rhino3dm,
+      three,
+      orbitControls,
+      rhino3dmLoader,
+      definition,
+      scene,
+      camera,
+      renderer,
+      doc
+    }
   },
   created() {
     console.log('Created')
   },
-  beforeMount() {
+  async beforeMount() {
     console.log('before Mounted')
+
+    if (THREE != null) {
+      this.three = THREE
+      console.log('THREE loaded')
+    }
+    if (OrbitControls != null) {
+      this.orbitControls = OrbitControls
+      console.log('OrbitControls loaded')
+    }
+    if (Rhino3dmLoader != null) {
+      this.rhino3dmLoader = Rhino3dmLoader
+      console.log('Rhino3dmLoader loaded')
+    }
     this.setServerAuth()
-    this.definition = this.getGrasshoperFile(import.meta.env.VITE_GRASSHOPPER_FILE_PATH)
-  },
-  async mounted() {
+    await this.getGrasshoperFile(import.meta.env.VITE_GRASSHOPPER_FILE_PATH)
     this.initThree()
     await this.compute()
   },
+  async mounted() {},
   methods: {
     collectResults(responseJson) {
-      const values = responseJson.values
+      var values = responseJson.values
 
       // clear doc
-      if (this.doc !== undefined) this.doc.delete()
+      if (this.doc != null || this.doc != undefined) this.doc.delete()
+      this.doc = new this.$rhino.File3dm()
 
       //console.log(values)
-      this.doc = new this.$rhino.File3dm()
 
       // for each output (RH_OUT:*)...
       for (let i = 0; i < values.length; i++) {
@@ -42,8 +72,9 @@ export default {
           // ...and for each branch...
           for (let j = 0; j < branch.length; j++) {
             // ...load rhino geometry into doc
-            const rhinoObject = this.decodeItem(branch[j])
-            if (rhinoObject !== null) {
+            var rhinoObject = this.decodeItem(branch[j])
+            console.log(rhinoObject)
+            if (rhinoObject != null) {
               this.doc.objects().add(rhinoObject, null)
             }
           }
@@ -56,32 +87,40 @@ export default {
       }
 
       // set up loader for converting the results to threejs
-      const loader = new this.$Rhino3dmLoader()
+      const loader = new this.rhino3dmLoader()
       loader.setLibraryPath('https://unpkg.com/rhino3dm@7.15.0/')
 
-      const resMaterial = new this.$THREE.MeshBasicMaterial({
+      const resMaterial = new this.three.MeshBasicMaterial({
         vertexColors: true,
         wireframe: true
       })
       // load rhino doc into three.js scene
       const buffer = new Uint8Array(this.doc.toByteArray()).buffer
-      loader.parse(buffer, function (object) {
-        // add material to resulting meshes
-        object.traverse((child) => {
-          child.material = resMaterial
-        })
+      let tempScene = this.scene
+      loader.parse(
+        buffer,
+        function (object) {
+          // add material to resulting meshes
+          object.traverse((child) => {
+            child.material = resMaterial
+          })
 
-        // add object graph from rhino model to three.js scene
-        this.scene.add(object)
-      })
+          // add object graph from rhino model to three.js scene
+          tempScene.add(object)
+        },
+        function (error) {
+          // handle the error here
+          console.error(error)
+        }
+      )
     },
     initThree() {
       // Rhino models are z-up, so set this as the default
-      this.$THREE.Object3D.DefaultUp = new this.$THREE.Vector3(0, 0, 1)
+      this.three.Object3D.DefaultUp = new this.three.Vector3(0, 0, 1)
 
-      this.scene = new this.$THREE.Scene()
-      this.scene.background = new this.$THREE.Color(1, 1, 1)
-      this.camera = new this.$THREE.PerspectiveCamera(
+      this.scene = new this.three.Scene()
+      this.scene.background = new this.three.Color(1, 1, 1)
+      this.camera = new this.three.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
         0.1,
@@ -92,13 +131,13 @@ export default {
       this.camera.position.y = 50
       this.camera.position.z = 25
 
-      this.renderer = new this.$THREE.WebGLRenderer({ antialias: true })
+      this.renderer = new this.three.WebGLRenderer({ antialias: true })
       this.renderer.setPixelRatio(window.devicePixelRatio)
       this.renderer.setSize(window.innerWidth, window.innerHeight)
       document.body.appendChild(this.renderer.domElement)
 
       // add some controls to orbit the this.camera
-      this.controls = new this.$OrbitControls(this.camera, this.renderer.domElement)
+      this.controls = new this.orbitControls(this.camera, this.renderer.domElement)
 
       window.addEventListener('resize', this.onWindowResize, false)
 
@@ -117,32 +156,38 @@ export default {
 
       const crvData = JSON.stringify(nCrv.encode())
 
-      const param1 = new this.$RhinoCompute.Grasshopper.DataTree('curve')
+      const param1 = new this.$rhinoCompute.Grasshopper.DataTree('curve')
       param1.append([0], [crvData])
 
       // clear values
       let trees = []
       trees.push(param1)
 
-      // Call RhinoCompute
+      // Call $rhinoCompute
 
-      const res = await this.$RhinoCompute.Grasshopper.evaluateDefinition(this.definition, trees)
+      const res = await this.$rhinoCompute.Grasshopper.evaluateDefinition(this.definition, trees)
 
       console.log(res)
 
       this.collectResults(res)
     },
     decodeItem(item) {
-      const data = JSON.parse(item.data)
+      var data = JSON.parse(item.data)
       if (item.type === 'System.String') {
         // hack for draco meshes
         try {
-          return this.$rhino.DracoCompression.decompressBase64String(data)
+          var parsedMesh = this.$rhino.DracoCompression.decompressBase64String(data)
+          return parsedMesh
         } catch (error) {
           console.log(error)
         } // ignore errors (maybe the string was just a string...)
       } else if (typeof data === 'object') {
-        return this.$rhino.CommonObject.decode(data)
+        try {
+          var geometry = this.$rhino.CommonObject.decode(data)
+          return geometry
+        } catch (error) {
+          console.log(error)
+        }
       }
       return null
     },
@@ -150,7 +195,8 @@ export default {
       let url = definitionName
       let res = await fetch(url)
       let buffer = await res.arrayBuffer()
-      return new Uint8Array(buffer)
+      this.definition = new Uint8Array(buffer)
+      console.log('definition loaded', this.definition)
     },
     onWindowResize() {
       this.camera.aspect = window.innerWidth / window.innerHeight
@@ -159,8 +205,9 @@ export default {
       this.animate()
     },
     setServerAuth() {
-      this.$RhinoCompute.authToken = import.meta.env.VITE_COMPUTE_AUTH_TOKEN
-      this.$RhinoCompute.url = import.meta.env.VITE_COMPUTE_SERVER_URL
+      this.$rhinoCompute.authToken = import.meta.env.VITE_COMPUTE_AUTH_TOKEN
+      this.$rhinoCompute.url = import.meta.env.VITE_COMPUTE_SERVER_URL
+      console.log('auth successful', this.$rhinoCompute)
     },
     animate() {
       requestAnimationFrame(this.animate)
